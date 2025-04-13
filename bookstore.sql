210e65b7a490fd9c62a885e5c5a7219f16e06c6f
Sospeter Owino <113516060+Itchy-Fingers@users.noreply.github.com>
2025-04-13 23:14:36 +0300

    Final project,contains the database and the tables

--- bookstore.sql
+++ bookstore.sql
@@ -34,17 +34,52 @@ CREATE TABLE book_author (
     FOREIGN KEY (book_id) REFERENCES book(book_id),
     FOREIGN KEY (author_id) REFERENCES author(author_id)
 );
+CREATE TABLE Customers (
+CustomerID INT PRIMARY KEY,
+CustomerName VARCHAR (100),
+Age int,
+Gender ENUM ('Male', 'Female', 'Other') 
+);
+
+CREATE TABLE Customer_Address (
+  AddressID INT PRIMARY KEY AUTO_INCREMENT,
+  CustomerID INT (50),
+  AddressLine VARCHAR(50),
+  City VARCHAR(100),
+  County VARCHAR(100),
+  PostalCode VARCHAR(20),
+  FOREIGN KEY (CustomerID) REFERENCES customers(CustomerID)
+);
+
+CREATE TABLE address_status (
+  AddressID INT PRIMARY KEY,
+  AddressStatus VARCHAR(20),
+  StatusDate VARCHAR(20),
+  FOREIGN KEY (AddressID) REFERENCES Customer_Address(AddressID)
+);
+
+CREATE TABLE Address (
+AddressID INT PRIMARY KEY,
+CustomerID INT (50),
+AddressLine VARCHAR(50),
+City VARCHAR(100),
+County VARCHAR(100),
+PostalCode VARCHAR(20),
+AddressStatus VARCHAR(20),
+FOREIGN KEY (AddressID) REFERENCES address_status(AddressID),
+FOREIGN KEY (CustomerID) REFERENCES customers(CustomerID)
+);
 
 -- Mysql querry to create table for the orders made by customers.
 CREATE TABLE customer_Orders(
 orderId INT PRIMARY KEY AUTO_INCREMENT,
 order_date VARCHAR(50),
 total_amount DECIMAL(10,2),
-customerId INT,
+customerID INT,
 addressId INT,
 shipping_method_Id INT,
 order_status_Id INT,
-FOREIGN KEY (customerId) REFERENCES customers(customerId),
+FOREIGN KEY (customerID) REFERENCES customers(customerID),
 FOREIGN KEY (addressId) REFERENCES customer_address(addressId),
 FOREIGN KEY (shipping_method_Id) REFERENCES shipping_methods(shipping_method_Id),
 FOREIGN KEY (order_status_Id) REFERENCES order_status(order_status_Id));
@@ -57,7 +92,7 @@ quantity INT,
 orderId INT,
 bookId INT,
 FOREIGN KEY (orderId) REFERENCES customer_Orders(orderId),
-FOREIGN KEY (bookId) REFERENCES books(bookID));
+FOREIGN KEY (book_id) REFERENCES book(book_id));
 
 -- Mysql querry to create table for the methods available for shipping the books.
 CREATE TABLE shipping_methods(
